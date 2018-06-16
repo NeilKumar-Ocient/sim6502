a829236531e51305921d93d551fb267661de1e3e
Austen Darre <adarre@ocient.com>
2018-06-16 16:53:12 +0000

    adding file for logger class that will be needed for debug purposes

---
+++ include/log.h
@@ -0,0 +1,8 @@
+#ifndef LOG_H
+#define LOG_H
+
+//TODO
+//a class for logging, intentionally not thread safe b/c no 2 threads should be logging to the same logger for this project
+//and also to increase performance in debug builds
+
+#endif

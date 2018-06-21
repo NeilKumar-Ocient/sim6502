ac4ad5cd042b239be85a356b4f7e0839ca8a17c1
NeilKumar-Ocient <nkumar@ocient.com>
2018-06-21 01:18:08 +0000

    added functionality to cpuClock constructor inside the constructor we loop n times to build a vector with wait times. Now when we finish an instruction early and have time to wait, we can just do this:
    say that timeToWait = 10;
    we can now just spin m_timeCycle[(timeToWait/10)+1] times

--- include/cpuClock.h
+++ include/cpuClock.h
@@ -3,8 +3,13 @@
 
 #include <ctime>
 #include <cassert>
+<<<<<<< d9bee61b977b6965f1eaca1b6bf71e7b6403d847
 #include <chrono>
 #include "log.h"
+=======
+#include <vector>
+#include <iostream>
+>>>>>>> added functionality to cpuClock constructor
 
 static constexpr size_t NANO =  1000UL * 1000UL * 1000UL;
 static constexpr size_t frequencyConversionFactor =  1000UL * NANO; //1 trillion nanoseconds divided by frequency in mHz gives us the period in nanoseconds
@@ -19,6 +24,48 @@ class cpuClock_t {
 public:
 	//constructor, takes in a frequency in mHz
 	cpuClock_t(size_t frequency, const logger_t& logger) : m_frequency(frequency), m_period(frequencyConversionFactor / m_frequency), LOGGER(logger) {}
+
+		//below code is used to initialize the vector we will use to determine how long to wait if we finish an instruction early
+		timeSpec_t startTime;
+		timeSpec_t endTime;
+		size_t timeDelta = 0;
+
+		for(size_t i = 1; i < 5000; i+=2) {
+			timeDelta=0;
+			size_t totalCycleSample = i * 1000000;
+			size_t cycleCount = 0;
+			clock_gettime(CLOCK_REALTIME, &startTime);
+			while(cycleCount < totalCycleSample) {
+				cycleCount++;
+			}
+			clock_gettime(CLOCK_REALTIME, &endTime);
+			timeDelta = timeElapsed(startTime, endTime)/1000000;
+			if(timeDelta/10 == (m_timeCycle.size()+1))
+			{
+				m_timeCycle.emplace_back(i);
+				//this will move to logger
+				std::cout << "added to cycleList" << std::endl;
+			} else if(timeDelta/10 > (m_timeCycle.size()+1)) {
+				//also move this to logger
+				std::cout << "numCycles: " << std::to_string(i) <<  "\ttimeElapsed: " << std::to_string(timeDelta) <<"\t currentValIndexWouldBe: " << std::to_string(timeDelta/10) <<  "\tTimeWeAreLookingFor: " << std::to_string(m_timeCycle.size()+1) << std::endl;
+				i-=2;	
+			} else {
+				//should we move this to a logger or remove the else case entirely?
+				std::cout << "numCycles: " << std::to_string(i) <<  "\ttimeElapsed: " << std::to_string(timeDelta) <<"\t currentValIndexWouldBe: " << std::to_string(timeDelta/10) <<  "\tTimeWeAreLookingFor: " << std::to_string(m_timeCycle.size()+1) << std::endl;
+			}
+
+			if((m_timeCycle.size()+1)*10 > m_period){
+				break;
+			}
+		}
+		//uncomment below to have it print list in constructor
+		/*
+		for(size_t i = 0; i < m_timeCycle.size(); i ++) {
+			std::cout << "time: " << std::to_string((i+1)*10) << "\tCycleCount: " << std::to_string(m_timeCycle[i]) << std::endl;
+		}
+		*/
+	}
+>>>>>>> added functionality to cpuClock constructor
 
 	//starts the clock at the current time
 	void start() {
@@ -58,6 +105,8 @@ private:
 	timeSpec_t m_checkPoint; //the time of the last checkpoint
 	size_t m_period; //the time delta between cycles determined by the frequency in nanoseconds
 	logger_t LOGGER; //the logger, is always copied from some other logger
+
+	std::vector<long> m_timeCycle;
 
 	//returns the number of nanoseconds between oldTime newTime
 	size_t timeElapsed(const timeSpec_t& oldTime, const timeSpec_t& newTime) {
